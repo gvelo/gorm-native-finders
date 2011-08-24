940f4d418bb8fb1ca90a8180e9ce2dc86f7e0abc
Gabriel Velo <gabriel.velo@gmail.com>
2011-08-24 12:00:12 +0000

    add HQL query cache

--- src/java/groovy/org/grails/plugin/nativefinders/NativeFinderHandler.java
+++ src/java/groovy/org/grails/plugin/nativefinders/NativeFinderHandler.java
@@ -2,6 +2,7 @@ package groovy.org.grails.plugin.nativefinders;
 
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.IdentityHashMap;
 import java.util.List;
 
 import org.codehaus.groovy.ast.expr.ClosureExpression;
@@ -22,37 +23,35 @@ import org.springframework.orm.hibernate3.HibernateTemplate;
  */
 public class NativeFinderHandler {
 
-	GrailsDomainClass domainClass;
-	SessionFactory sessionFactory;
+	private GrailsDomainClass domainClass;
+	private SessionFactory sessionFactory;
+	private IdentityHashMap<ClosureExpression, String> queryCache;
 
-	public NativeFinderHandler( GrailsDomainClass domainClass, SessionFactory sessionFactory ) {
+	public NativeFinderHandler(GrailsDomainClass domainClass, SessionFactory sessionFactory) {
 		this.domainClass = domainClass;
 		this.sessionFactory = sessionFactory;
+		queryCache = new IdentityHashMap<ClosureExpression, String>();
 	}
 
 	public Object find(ClosureExpression closureExpression, final ArrayList parameters) {
 
-		HibernateTemplate hibernateTemplate = new HibernateTemplate( sessionFactory );
+		HibernateTemplate hibernateTemplate = new HibernateTemplate(sessionFactory);
 
-		Closure2HQL closure2HQL = new Closure2HQL();
+		final String query = getQuery(domainClass.getName(), closureExpression);
 
-		// TODO: as closureExpression is cached in the caller's class , we could use a identity
-		// map to cache the generated queries to avoid string build overhead.
-
-		final String query = closure2HQL.tranformClosureExpression( domainClass.getName(), closureExpression );
-
-		return hibernateTemplate.execute( new HibernateCallback<Object>() {
+		return hibernateTemplate.execute(new HibernateCallback<Object>() {
 			public Object doInHibernate(Session session) throws HibernateException, SQLException {
 
-				Query q = session.createQuery( query );
+				Query q = session.createQuery(query);
 
 				for (int i = 0; i < parameters.size(); i++) {
-					q.setParameter( i, parameters.get( i ) );
+					q.setParameter(i, parameters.get(i));
 				}
 
 				// only want one result, could have used uniqueObject here
-				// but it throws an exception if its not unique which is undesirable
-				q.setMaxResults( 1 );
+				// but it throws an exception if its not unique which is
+				// undesirable
+				q.setMaxResults(1);
 
 				// TODO: add support for q.setCacheable()
 				// q.setCacheable(useCache);
@@ -60,40 +59,59 @@ public class NativeFinderHandler {
 				List results = q.list();
 
 				if (results.size() > 0) {
-					return GrailsHibernateUtil.unwrapIfProxy( results.get( 0 ) );
+					return GrailsHibernateUtil.unwrapIfProxy(results.get(0));
 				}
 
 				return null;
 
 			}
-		} );
+		});
 
 	}
 
 	public Object findAll(ClosureExpression closureExpression, final ArrayList parameters) {
 
-		HibernateTemplate hibernateTemplate = new HibernateTemplate( sessionFactory );
+		HibernateTemplate hibernateTemplate = new HibernateTemplate(sessionFactory);
 
-		Closure2HQL closure2HQL = new Closure2HQL();
+		final String query = getQuery(domainClass.getName(), closureExpression);
 
-		// TODO: as closureExpression is cached in the caller's class , we could use a identity
-		// map to cache the generated queries to avoid string build overhead.
-
-		final String query = closure2HQL.tranformClosureExpression( domainClass.getName(), closureExpression );
-
-		return hibernateTemplate.executeFind( new HibernateCallback<Object>() {
+		return hibernateTemplate.executeFind(new HibernateCallback<Object>() {
 			public Object doInHibernate(Session session) throws HibernateException, SQLException {
 
-				Query q = session.createQuery( query );
+				Query q = session.createQuery(query);
 
 				for (int i = 0; i < parameters.size(); i++) {
-					q.setParameter( i, parameters.get( i ) );
+					q.setParameter(i, parameters.get(i));
 				}
 
 				return q.list();
 
 			}
-		} );
+		});
+
+	}
+
+	private String getQuery(String className, ClosureExpression closureExpression) {
+
+		String query = null;
+
+		synchronized (queryCache) {
+			query = queryCache.get(closureExpression);
+		}
+
+		if (query == null) {
+
+			Closure2HQL closure2HQL = new Closure2HQL();
+
+			query = closure2HQL.tranformClosureExpression(className, closureExpression);
+
+			synchronized (queryCache) {
+				queryCache.put(closureExpression, query);
+			}
+
+		}
+
+		return query;
 
 	}
 
