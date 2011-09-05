7f504dccde47d68cb934ae8a3342191d1727ace9
Gabriel Velo <gabriel.velo@gmail.com>
2011-09-05 02:46:04 +0000

    add license headers to source files.

--- src/ASTTransformation/org/grails/plugin/nativefinders/transform/NativeFinderTransform.java
+++ src/ASTTransformation/org/grails/plugin/nativefinders/transform/NativeFinderTransform.java
@@ -1,3 +1,20 @@
+/*
+* Copyright (c) 2011 the original author or authors.
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+
 package org.grails.plugin.nativefinders.transform;
 
 import groovyjarjarasm.asm.Opcodes;
@@ -21,7 +38,6 @@ import org.codehaus.groovy.ast.expr.Expression;
 import org.codehaus.groovy.ast.expr.ListExpression;
 import org.codehaus.groovy.ast.expr.MethodCallExpression;
 import org.codehaus.groovy.ast.expr.VariableExpression;
-import org.codehaus.groovy.control.CompilationUnit;
 import org.codehaus.groovy.control.CompilePhase;
 import org.codehaus.groovy.control.SourceUnit;
 import org.codehaus.groovy.syntax.Token;
